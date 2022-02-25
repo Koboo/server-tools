e5de06f46d4155a8f56f28400b4b952fb9e1cee5
Koboo <14143908+Koboo@users.noreply.github.com>
2022-02-25 15:03:02 +0100

    Create install_corretto11.sh

---
+++ scripts/install_corretto11.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function RunChecks {
+    if [ "`id -u`" != "0" ]; then
+        echo "You are not root! Abort."
+        exit
+    fi
+}
+
+function DownloadAndPrepare {
+    wget https://corretto.aws/downloads/latest/amazon-corretto-11-x86-linux-jdk.tar.gz -P /usr/share -O /usr/share/corretto11.tar.gz
+    mkdir /usr/share/corretto11
+}
+
+function Install {
+    tar -C /usr/share/corretto11 -xvzf /usr/share/corretto11.tar.gz
+    mv /usr/share/corretto11/amazon-corretto-11.0.14.10.1-linux-x86/* /usr/share/corretto11/
+}
+
+function RunLastStep {
+    rm -r /usr/share/corretto11/amazon-corretto-11.0.14.10.1-linux-x86/
+    update-alternatives --install /usr/bin/java java /usr/share/corretto11/bin/java 6
+}
+
+RunChecks
+DownloadAndPrepare
+Install
+RunLastStep
