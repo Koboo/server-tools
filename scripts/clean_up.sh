20f64b2d61e38758e15276416a0d681953847a53
Koboo <14143908+Koboo@users.noreply.github.com>
2022-04-14 09:25:00 +0200

    Update and rename run_clean_up.sh to clean_up.sh

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -6,14 +6,21 @@ function RunChecks {
     fi
 }
 
+function RemoveOldKernels {
+    sudo apt-get purge $( sudo dpkg --list | sudo egrep 'linux-image-[0-9]' | sudo awk '{print $3,$2}' | sudo sort -nr | sudo tail -n +2 | sudo grep -v $(uname -r) | sudo awk '{ print $2}')
+    echo "Removed old kernels.."
+}
+
 function CleanUp {
     sudo aptitude search ~c
     sudo aptitude purge ~c
     sudo apt-get autoremove
     sudo apt-get autoclean
+    sudo apt-get clean
     echo "Finished cleanup.."
 }
 
 
 RunChecks
+RemoveOldKernels
 CleanUp
