e0fc703d231ae3fd06c92ed10f1176c36599b729
Koboo <14143908+Koboo@users.noreply.github.com>
2022-11-17 11:58:30 +0100

    Update clean_up.sh

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -7,16 +7,16 @@ function RunChecks {
 }
 
 function RemoveOldKernels {
-    sudo apt-get purge $( sudo dpkg --list | sudo egrep 'linux-image-[0-9]' | sudo awk '{print $3,$2}' | sudo sort -nr | sudo tail -n +2 | sudo grep -v $(uname -r) | sudo awk '{ print $2}')
+    sudo apt-get purge $( sudo dpkg --list | sudo egrep 'linux-image-[0-9]' | sudo awk '{print $3,$2}' | sudo sort -nr | sudo tail -n +2 | sudo grep -v $(uname -r) | sudo awk '{ print $2}') -y
     echo "Removed old kernels.."
 }
 
 function CleanUp {
     sudo aptitude search ~c
     sudo aptitude purge ~c
-    sudo apt-get autoremove
-    sudo apt-get autoclean
-    sudo apt-get clean
+    sudo apt-get autoremove -y
+    sudo apt-get autoclean -y
+    sudo apt-get clean -y
     echo "Finished cleanup.."
 }
 
